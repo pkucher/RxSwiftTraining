64c1ea24408398c55c489a67c7d4f5bbab80142f
Paulo <paulokucher@brq.com>
2019-06-18 16:49:26 -0300

    Initial Commit

---
+++ RxTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxTraining
+//
+//  Created by brq on 18/06/2019.
+//  Copyright © 2019 brq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
