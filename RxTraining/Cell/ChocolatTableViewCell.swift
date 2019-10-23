9405f128ffcf175b7711ca2ecefc2466a94cee3d
Paulo <paulokucher@brq.com>
2019-10-23 10:54:05 -0300

    criado o repositorio

---
+++ RxTraining/Cell/ChocolatTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChocolatTableViewCell.swift
+//  RxTraining
+//
+//  Created by brq on 18/06/2019.
+//  Copyright © 2019 brq. All rights reserved.
+//
+
+import UIKit
+
+class ChocolatTableViewCell: UITableViewCell {
+    
+    var title: String = ""
+    var imageSweet = UIImageView()
+    var imageFlag = UIImageView()
+    var priceLabel: String = ""
+    
+}
